880e3b9791c4285718301897fe8ca1e5daf9f968
Your Name <ponnamrishwanth78@gmail.com>
2025-03-05 01:49:21 +0530

    this basic

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///club.db'
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+
+db = SQLAlchemy(app)
+
+# Database Model
+class Member(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+
+db.create_all()
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/events')
+def events():
+    return render_template('events.html')
+
+@app.route('/members')
+def members():
+    return render_template('members.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
